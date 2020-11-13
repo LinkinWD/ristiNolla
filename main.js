91a10baf7dcab23b965702c7eb091af8ee5b1273
unknown <yaramodesu@gmail.com>
2020-11-13 14:50:04 +0200

    voittajan ratkaiseminen vielä kesken

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ const btn = document.querySelector('button')
 const overlay = document.querySelector('.overlay')
 const modal = document.querySelector('.modal')
 
+const voittoRivit = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+let ratkaisija = 0
 let turn = 0
 //pelaaja 1 on punainen
 let pelaaja = 1
@@ -23,24 +30,78 @@ ruutu.forEach(function(ele) {
                 vuoro.innerText = 'punaisen'
                 vuoro.style.color = 'red'
             }
-               
+               tarkistaPunainen()
+               tarkistaVihreä()
                turn++
                 if(turn == 9) {
-                    console.log('loppu')
+                    modal.classList.add('end')
                     overlay.classList.add('end')
-                    modal.style.display = 'block'
-                    turn = 0
+                    voittaja.innerText ='Tasapeli'
+                    overlay.addEventListener('click', () =>{
+                        reset()
+                    })
+                   
+                   
+                   
+                    
                 }    
             })
     
 } )
 
+btn.addEventListener('click', () => {
+    reset()
+})
 
+function reset() {
+    turn = 0
+    ruutu.forEach((e) => {
+        e.innerHTML = ''
+        voittaja.innerText = ''
+        vuoro.innerText = 'Punainen aloittaa'
+        vuoro.style.color = 'black'
+        modal.classList.remove('end')
+        overlay.classList.remove('end')
+        voittaja.innerText = ''
+        let ratkaisija = 0
+    })
+}
 
-/* 
-if(!turn % 2) {
-    ele.innerHTML = '<p>x</p>'
-    turn ++
-    console.log(turn)
-    return turn
-} */
+function tarkistaPunainen(){ 
+    let ratkaisija = 0
+   for(let numero of ruutu){ 
+   
+    for(i = 0; i < voittoRivit.length; i++) {       
+        for(let sisältö of voittoRivit[i]){            
+            let daata = numero.dataset.id       
+            if(numero.innerText == 'x' && parseInt(daata) == sisältö){
+                ratkaisija++
+                if(ratkaisija == 3) {
+                    console.log('punainen')
+                }  
+            }  
+                   
+            } 
+        }
+    }        
+    }
+    function tarkistaVihreä(){ 
+        let ratkaisija = 0 
+       for(let numero of ruutu){ 
+        for(i = 0; i < voittoRivit.length; i++) { 
+                 
+            for(let sisältö of voittoRivit[i]){            
+                let daata = numero.dataset.id       
+                if(numero.innerText == 'o' && parseInt(daata) == sisältö){
+                    ratkaisija++
+                    if(ratkaisija == 3) {
+                        console.log('vihreä')
+                    }  
+                }  
+                       
+                } 
+            }
+        }        
+        }  
+ 
+
